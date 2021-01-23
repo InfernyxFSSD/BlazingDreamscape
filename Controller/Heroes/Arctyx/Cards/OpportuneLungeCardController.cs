0a9110aab9b77eea370fcf900677038879057ee2
Infernyx <flameslash@live.com>
2021-01-23 18:09:39 -0500

    Small fixes

--- Controller/Heroes/Arctyx/Cards/OpportuneLungeCardController.cs
+++ Controller/Heroes/Arctyx/Cards/OpportuneLungeCardController.cs
@@ -7,15 +7,12 @@ using Handelabra.Sentinels.Engine.Model;
 
 namespace SybithosInfernyx.Arctyx
 {
-	// Token: 0x020007CF RID: 1999
 	public class OpportuneLungeCardController : CardController
 	{
-		// Token: 0x06003437 RID: 13367 RVA: 0x0002422C File Offset: 0x0002242C
 		public OpportuneLungeCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
 		{
 		}
 
-		// Token: 0x06003438 RID: 13368 RVA: 0x0007D011 File Offset: 0x0007B211
 		public override IEnumerator Play()
 		{
 			IEnumerator coroutine = base.GameController.SelectTargetsAndDealDamage(this.DecisionMaker, new DamageSource(base.GameController, base.CharacterCard), 2, DamageType.Melee, new int?(1), false, new int?(1), false, false, false, null, null, null, null, null, false, null, null, false, null, base.GetCardSource(null));
